33524df2dcfeaf9014d6e3b36a58766b1d388827
nyrswwiq <nastyakhila@gmail.com>
2026-05-26 12:16:03 +0600

    Полная ПР6

--- 922-master/Практическая работа № 6/Задание_4/sample/index.php
+++ 922-master/Практическая работа № 6/Задание_4/sample/index.php
@@ -10,43 +10,53 @@
 	<h2>Пользовательские функции</h2>
 	
 	<?php
-		// подключаем функцию fnGetData()
 		require 'function.php';	
-		// получаем возвращаемый функцией массив
 		$data = fnGetData();
 		
-		// echo "<pre>";
-		// var_dump($data);
-		// echo "</pre>";
-
 		// забираем данные по категории
 		$person = $data["personnel"];
 		$courses = $data["courses"];
 		$educations = $data["educations"];
 		
+		// Функция вывода персональных данных
 		function getPersonData($data) {
-			// ...
-			
+			echo "<h3>Персональные данные</h3>";
+			echo "<p>ФИО: {$data['surname']} {$data['name']} {$data['patronymic']}</p>";
+			echo "<p>Должность: {$data['post']}</p>";
+			echo "<p>Категория: {$data['category']}</p>";
+			echo "<p>Общий стаж: {$data['experience_total']} лет</p>";
+			echo "<hr>";
 		};
 		
+		// Функция вывода образования
 		function getPersonEdu($data) {
-			// ...	
-			
+			echo "<h3>Образование</h3>";
+			foreach ($data as $edu) {
+				echo "<p><strong>Учебное заведение:</strong> {$edu['institution']}</p>";
+				echo "<p><strong>Квалификация:</strong> {$edu['qualification']}</p>";
+				echo "<p><strong>Годы:</strong> {$edu['year_receipts']} - {$edu['year_release']}</p>";
+				echo "<hr>";
+			}
 		};
 		
+		// Функция вывода курсов
 		function getPersonCours($data) {
-			// ...
-			
+			echo "<h3>Курсы</h3>";
+			foreach ($data as $course) {
+				echo "<p><strong>Название:</strong> {$course['name']}</p>";
+				echo "<p><strong>Длительность:</strong> {$course['duration']} часов</p>";
+				echo "<p><strong>Цена:</strong> {$course['price']} руб.</p>";
+				echo "<hr>";
+			}
 		}
 		
 		// выводим персональные данные
-		echo getPersonData($person);
+		getPersonData($person);
 		// выводим данные об образовании
-		echo getPersonEdu($educations);
+		getPersonEdu($educations);
 		// выводим данные о курсах
-		echo getPersonCours($courses);
+		getPersonCours($courses);
 	?>
 	
-
 </body>
 </html>
